3ed95de8ad7e7ea1f81eb647ef855b0a0cda5f0f
Mahfuz Mia <sayhello.mahfuz@gmail.com>
2024-07-30 23:25:07 +0600

    update notes.txt

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('users/', include('djoser.urls')),
+    path('users/', include('djoser.urls.authtoken')),
     path('menu-items/', views.MenuItemListView.as_view()),
     path('menu-items/category/', views.CategoryView.as_view()),
     path('menu-items/<int:pk>/', views.MenuItemDetailView.as_view()),
-    path('groups/managers/users/', views.ManagersListView.as_view()),
-    path('groups/managers/users/<int:pk>/', views.ManagersRemoveView.as_view()),
+    path('groups/manager/users/', views.ManagersListView.as_view()),
+    path('groups/manager/users/<int:pk>/', views.ManagersRemoveView.as_view()),
     path('groups/delivery-crew/users/', views.DeliveryCrewListView.as_view()),
     path('groups/delivery-crew/users/<int:pk>/', views.DeliveryCrewRemoveView.as_view()),
     path('cart/menu-items/', views.CartOperationsView.as_view()),
